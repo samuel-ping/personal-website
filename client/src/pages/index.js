c9b137090f3e09d4da97899bbdd0ef2e69aae35e
Sam Ping <samuelping42@gmail.com>
2021-05-26 23:36:05 -0400

    Add custom colors

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -16,7 +16,8 @@ export default function Home({ taglines }) {
         />
         <h1 className="leading-relaxed text-6xl font-bold">
           Hi! I&rsquo;m{' '}
-          <span className="text-green-600 text-shadow">Sam Ping</span>, a...
+          <span className="text-shadow text-dark-spring-green">Sam Ping</span>,
+          a...
         </h1>
         <ul className="flex flex-col items-center">
           {taglines.map((tagline) => (
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /* eslint-disable global-require */
+const { colors } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: {
     enabled: true, // without this, it purges all of my CSS D:
@@ -12,6 +14,36 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      // Defaults
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      green: colors.green,
+      blue: colors.blue,
+      // Custom Colors
+      // Taken from Valspar and Coolors.co and mixed them around
+      // Greens
+      'vegas-green': '#407148',
+      wilderness: '#476151',
+      'dark-wilderness': '#2b4f39',
+      'dark-spring-green': '#157145',
+      'forest-green-crayola': '#57A773',
+      'prickly-pear': '#798B74',
+      'pistachio-ice-green': '#abc6a7',
+      'ballroom-dancing': '#A9B4A5',
+      'lazy-days': '#D0E4D8',
+      'mint-hint': '#DFF2E1',
+      // Yellows
+      beehive: '#F9CC7F',
+      corn: '#f4e666',
+      'laser-lemon': '#f7fe72',
+      // Blues
+      'quarry-pond': '#436379',
+      'carolina-skies': '#B2CAD4',
+    },
     extend: {
       // tailwind-typography settings (unused at the moment)
       typography: {
